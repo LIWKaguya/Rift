a87d40e9958918bfd7978a94bfd853c8418ca1da
LIWKaguya <hungle120901@gmail.com>
2022-04-25 23:47:03 +0300

    Temporary finished with the backend

--- rift_backend/app.js
+++ rift_backend/app.js
@@ -1,7 +1,37 @@
 const express = require('express')
-
-const config = require('./utils/config')
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
 
 const app = express()
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/login', loginRouter)
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ rift_backend/controllers/blogs.js
@@ -0,0 +1,75 @@
+const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getToken = (req) => {
+	const authorization = req.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('using ')) {
+		return authorization.substring(6)
+	}
+	return null
+}
+
+blogsRouter.get('/', async(_, res) => {
+	const blogs = await Blog.find({}).populate('user')
+
+	res.json(blogs)
+})
+
+blogsRouter.get('/:id', async (req, res) => {
+	const blog = await Blog.findById(req.params.id)
+
+	if(blog) {
+		res.json(blog.toJSON())
+	} else {
+		res.status(404).end()
+	}
+})
+
+blogsRouter.post('/', async (req, res) => {
+	const { content } = req.body
+    
+	const token = getToken(req)
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+	if(!token || !decodedToken.id) {
+		return res.status(401).json({
+			error: 'token missing or invalid'
+		})
+	}
+
+	const user = await User.findById(decodedToken.id)
+
+	const blog = new Blog({
+		content, 
+		likes: 0,
+		user: user._id,
+		comments: []
+	})
+
+	const savedBlog = await blog.save()
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
+
+	res.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+	await Blog.findByIdAndRemove(req.params.id)
+	res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+	const { content } = req.body
+	const foundBlog = await Blog.findById(req.params.id)
+	const blog = {
+		...foundBlog,
+		content
+	}
+
+	const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {new: true})
+	res.json(updatedBlog)
+})
+
+module.exports = blogsRouter
