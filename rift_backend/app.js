b8433caf13e99280db15b8bd80673b3d80a1eb0d
LIWKaguya <hungle120901@gmail.com>
2022-04-25 21:45:05 +0300

    Adding users and login controllers

---
+++ rift_backend/app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const config = require('./utils/config')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
---
+++ rift_backend/controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async(_, res) => {
+	const users = await User.find({}).populate('blogs')
+
+	res.json(users)
+})
+
+usersRouter.post('/', async (req, res) => {
+	const {username, name, password} = req.body
+
+	const existed = await User.findOne({ username })
+	if(existed) {
+		return res.status(400).json({
+			error: 'username must be unique'
+		})
+	}
+
+	const saltRounds = 20
+	const passwordHash = await bcrypt.hash(password, saltRounds)
+
+	const user = new User({
+		username, 
+		name,
+		passwordHash
+	})
+
+	const savedUser = await user.save()
+
+	res.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
